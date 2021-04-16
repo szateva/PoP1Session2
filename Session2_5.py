705f0fe1e9ef6f6507943954bed6b34d54e41fd7
Eva Szatmari <szateva@gmail.com>
2021-04-16 17:15:50 +0100

    Session2_5 solved

--- Session2_5.py
+++ Session2_5.py
@@ -12,3 +12,14 @@ TESTS:
 Input: 1 2 0 Output: 1
 Input: 6 8 9 2 0 Output: 6
 Input: 7 6 1 2 3 6 10 0 Output: 5 """
+
+num = int(input("Type in the sequence numbers, type 0 when finished: "))
+total = 0
+count = 0
+
+while num != 0:
+    total += num
+    count += 1
+    num = int(input("Type in the sequence numbers, type 0 when finished: "))
+a = int(total/count)
+print(a)
