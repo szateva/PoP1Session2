de837009d78e7139ad7531a8faf18ce34afb1918
Eva Szatmari <szateva@gmail.com>
2021-04-17 17:35:34 +0100

    Session2_12 solved

--- Session2_12.py
+++ Session2_12.py
@@ -15,8 +15,6 @@ def max_three(num1, num2, num3):
 
 n = max_three(4,2,5)
 
-printNo
-
 TESTS:
 Case 1:
 max_three(4,2,5) must be 5
@@ -26,3 +24,24 @@ Case 3:
 max_three(8,12,11) must be 12
 Case 4:
 max_three(9,2,9) must be 9 """
+
+def max_three(num1, num2, num3):
+    largest = num1
+    if num2 > largest:
+        largest = num2
+    elif num3 > largest:
+        largest = num3
+    return largest
+
+a = max_three(4,2,5)
+print(a)
+print("should be 5")
+b = max_three(7,2,1)
+print(b)
+print("should be 7")
+c = max_three(8,12,11)
+print(c)
+print("should be 12")
+d = max_three(9,2,9)
+print(d)
+print("should be 9")
